9635dd8b689f440b2daa3ef8fbe35df58c3dacf0
Veeraragavan V <vpnveera4@gmail.com>
2026-09-21 18:57:32 +0530

    Marketing bulk msg

---
+++ backend/alembic/versions/f8a9b0c1d2e3_add_meta_tier_limit_to_workspaces.py
@@ -0,0 +1,22 @@
+"""Add meta_tier_limit to workspaces.
+
+Revision ID: f8a9b0c1d2e3
+Revises: ('c9e2f4a6b803', 'h5c6d7e8f9g0')
+Create Date: 2026-09-21
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'f8a9b0c1d2e3'
+down_revision = ('c9e2f4a6b803', 'h5c6d7e8f9g0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('workspaces', sa.Column('meta_tier_limit', sa.Integer(), nullable=True))
+
+
+def downgrade():
+    op.drop_column('workspaces', 'meta_tier_limit')
